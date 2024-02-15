177c9096c6874f8da7da692cdf8a2e4f7fbe4e6f
yangtory <wjdduscldrn@naver.com>
2024-02-15 23:09:28 +0900

    메모완성

--- nodejs_900_Memo/public/js/detail.js
+++ nodejs_900_Memo/public/js/detail.js
@@ -1,12 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn_box = document.querySelector("div.btn");
+document.addEventListener('DOMContentLoaded', () => {
+    const btn_box = document.querySelector('div.btn');
 
-  btn_box.addEventListener("click", (e) => {
-    const target = e.target;
-    const m_seq = target.dataset.num;
-    alert(target.tagName);
-    if (target.className === "update") {
-      document.location.href = `/${m_seq}/update`;
-    }
-  });
+    btn_box.addEventListener('click', (e) => {
+        const target = e.target;
+        const m_seq = target.dataset.num;
+        if (target.className === 'update') {
+            document.location.href = `/${m_seq}/update`;
+        }
+        if (target.className === 'delete') {
+            if (confirm('정말 삭제할까요?')) {
+                document.location.href = `/${m_seq}/delete`;
+            }
+        }
+    });
 });
--- nodejs_900_Memo/public/js/main.js
+++ nodejs_900_Memo/public/js/main.js
@@ -1,11 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const list = document.querySelector("div.list");
+document.addEventListener('DOMContentLoaded', () => {
+    const list = document.querySelector('div.list');
+    const home = document.querySelector('h1');
 
-  list.addEventListener("click", (e) => {
-    const target = e.target;
-    if (target.tagName === "UL") {
-      const m_seq = target.dataset.num;
-      document.location.href = `/${m_seq}/detail`;
-    }
-  });
+    list.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.tagName === 'UL') {
+            const m_seq = target.dataset.num;
+            document.location.href = `/${m_seq}/detail`;
+        }
+    });
+
+    home.addEventListener('click', () => {
+        document.location.href = '/';
+    });
 });
